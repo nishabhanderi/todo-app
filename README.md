c6e7e4d0208e07e3fd54e344aad11ae61caec555
nishabhanderi <nishabhanderi27@gmail.com>
2026-03-22 19:39:16 +0530

    Added JWT authentication

--- README.md
+++ README.md
@@ -21,8 +21,32 @@ It allows users to create, view, update, complete, and delete tasks.
 - MongoDB
 - Mongoose
 
----
 
+---
+##  Authentication
+
+This project uses JSON Web Token (JWT) for secure authentication and authorization.
+
+### Features:
+- User Signup & Login
+- Password hashing using bcrypt
+- JWT token generation on login
+- Protected routes using middleware
+- Token-based authentication system
+
+### How it works:
+1. User logs in with email & password
+2. Server verifies credentials
+3. JWT token is generated
+4. Token is sent to client
+5. Client sends token in headers for protected routes
+
+### Authorization:
+- Only authenticated users can access protected APIs
+- Token is verified using middleware
+
+### Example Header:
+Authorization: Bearer <your_token>
 ##  Installation
 
 ```bash
--- middleware/auth.js
+++ middleware/auth.js
@@ -1,22 +1,25 @@
 const jwt = require("jsonwebtoken");
 
-  function auth(req,res,next){
-   const authHeader = req.headers.authorization;
+function auth(req, res, next) {
+    const authHeader = req.headers.authorization;
 
-
-    if (!authHeader || !authHeader.startsWith("Bearer")){
-        return res.status(401).json({message:"Token missing"});
+    // Bearer + space check
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+        return res.status(401).json({ message: "Token missing" });
     }
-     const token = authHeader.split(" ")[1];
-        
 
-    try{
-        const decoded = jwt.verify(token,process.env.JWT_SECRET);
-        req.user = decoded
+    //  Token extract
+    const token = authHeader.split(" ")[1];
+
+    try {
+        // Verify token
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        req.user = decoded;
         next();
+    } catch (err) {
+        return res.status(401).json({ message: "Invalid token" });
     }
-    catch(err){
-        return res.status(401).json({message:"Invalid token"});
-    };
-};
+}
+
 module.exports = auth;
